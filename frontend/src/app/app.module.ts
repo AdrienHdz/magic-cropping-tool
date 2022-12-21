0c5e366a20c735e7f4ef84a813d29a86dd601e95
AdrienHdz <adrien.hernandez@hec.ca>
2022-12-21 00:03:31 -0500

    feat: uploaded app module with all components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UploadComponent } from './upload/upload.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { UserModule } from './user/user.module';
+import { EventBlockerDirective } from './shared/directives/event-blocker.directive';
+import { DimensionsFormComponent } from './dimensions-form/dimensions-form.component';
+import { InputComponent } from './input/input.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    HomeComponent,
+    NotFoundComponent,
+    UploadComponent,
+    FooterComponent,
+    EventBlockerDirective,
+    DimensionsFormComponent,
+    InputComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    UserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
